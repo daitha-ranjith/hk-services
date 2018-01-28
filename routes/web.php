ef899389d8a22fe6eb1d185d1a24f8ce2f1a2e9d
Santosh Baggam <santoshbaggam@gmail.com>
2018-01-29 01:34:33 +0530

    comment actual demo

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('accounts', 'AccountController');
     Route::resource('tokens', 'TokenController');
     Route::put('reset-api-token', 'TokenController@resetApiToken')->name('reset-api-token');
-
-    Route::get('demo', function () {
-        $token = '1vLfhTU5MhVLfEou5pK0F3Ra5AFTJnvpA4qcsUmHR7LXVV9LRx9wf4GKbvTt';
-        $url = "https://healthkon-video-api.herokuapp.com/api/authorize?api_token=" . $token;
-        $data = json_decode(file_get_contents($url));
-
-        return view('demo.conference')->withToken($data->token);
-    });
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
@@ -26,3 +18,11 @@ Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
 });
 
 Route::view('demo', 'demo.conference');
+
+// Route::get('demo', function () {
+//     $token = '1vLfhTU5MhVLfEou5pK0F3Ra5AFTJnvpA4qcsUmHR7LXVV9LRx9wf4GKbvTt';
+//     $url = "https://healthkon-video-api.herokuapp.com/api/authorize?api_token=" . $token;
+//     $data = json_decode(file_get_contents($url));
+
+//     return view('demo.conference')->withToken($data->token);
+// });
