f4136b4f905d157ea3833049049cd257f66c1853
Santosh Baggam <santoshbaggam@gmail.com>
2018-03-06 17:50:17 +0530

    change default bitrate to 72 in video demo

--- resources/views/demo/video.blade.php
+++ resources/views/demo/video.blade.php
@@ -159,7 +159,7 @@
                     presenterInitiation: '{{ request('presenter') ?: false }}',
                     presenterIdentity: '{{ request('presenter') }}',
                     presenterVideoContainer: '#presenter-video-container',
-                    frameRate: {{ request('bitrate') ?: 5  }},
+                    frameRate: {{ request('bitrate') ?: 72 }},
                     width: 144,
                     duration: 3600,
                     record: true
